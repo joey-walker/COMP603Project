3beae363084376a6a528d2ec08f09020df456513
Joey Walker <walkerj1@wit.edu>
2015-07-13 23:22:38 -0400

    updated Readme, added .gitignore

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,17 @@
+/*
+COMP603PROJECT:
+Dreadnought Khamzhor: Prelude to Space and Time
+
+Authors:
+Joey Walker
+Deep Patel
+Ori Maci
+Sarthak Khatiwada
+Mentu Singh
+
+*/
+
 fn main() {
+
     println!("Hello, world!");
 }
