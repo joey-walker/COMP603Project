20ac65d93ece2e3d4da34a56b8f45ec16b0e1f1b
Joey Walker <walkerj1@wit.edu>
2015-07-24 00:31:59 -0400

    Refining tokens

--- pygame/game.py
+++ pygame/game.py
@@ -2,23 +2,29 @@
 # Dreadnought Khamzor: A prelude to Time and Space
 #
 # Authors: Joseph Walker, Sarthak Khatiwada, Ori Maci, Deep Patel, Chaskarandeep Singh
+#
+#	Made using ply http://www.dabeaz.com/ply/
 # -----------------------------------------------------------------------------
 
 
 #Our different token categories
 tokens = (
-    'MOVE', 'TALK',
+    'MOVE', 'TALK', 'LOOK', 'JOIN', 'WHITESPACE'
     )
 		
 # Our tokens
-t_MOVE		= r'move'
-
+t_MOVE			= r'move|run|walk'
+t_TALK			= r'talk|speak|chat|tell'
+t_LOOK			= r'look|glance'
+t_JOIN			= r'to|at|and' # I walked to him
+t_WHITESPACE	= r'\s'
 
 #Lexing errors
 def t_error(t):
 	print("error: bad characters: '%s'" % t.value[0])
 	t.lexer.skip(1)
 
+# Example class
 class Human(object):
 	pass		
 		
@@ -30,6 +36,8 @@ import ply.yacc as yacc
 #yacc.yacc()
 s = input("Action > ")
 lexer.input(s)
+
+
 while True:
     tok = lexer.token()
     if not tok: 
