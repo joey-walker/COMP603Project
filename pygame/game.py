46e3077768d3113196e817feaf4087f3e6e57962
Joey Walker <walkerj1@wit.edu>
2015-07-27 01:55:38 -0400

    just comments to indicated where we should go next for the group

--- pygame/game.py
+++ pygame/game.py
@@ -1,17 +1,27 @@
-# -----------------------------------------------------------------------------
-# Dreadnought Khamzor: A prelude to Time and Space
+# -------------------------------------------------------------------------------------
+# Dreadnought Khamzor: A prelude to Space and Time
 #
 # Authors: Joseph Walker, Sarthak Khatiwada, Ori Maci, Deep Patel, Chaskarandeep Singh
 #
 #	Made using ply http://www.dabeaz.com/ply/
-# -----------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
 
 import sys
 
 # Example class
+# Human -> Player, should hold onto a inventory(if we want), location of player
 class Human(object):
 	pass
-	
+
+"""
+Room -> The areas of the map, should contain connectors to other rooms, 
+text of objects in room, functions determining state of room/interactivity,
+Should contain the state of the room determinig player progress in the area, eg.
+until player completes puzzle, connector remains locked.
+"""
+class Room(object):
+	pass
+
 #Our different token categories
 tokens = (
     'MOVE', 'TALK', 'LOOK', 'JOIN', 'WHITESPACE', 'QUIT', 'INV'
@@ -24,6 +34,15 @@ t_LOOK			= r'look|glance'
 t_JOIN			= r'to|at|and' # I walked to him
 t_WHITESPACE	= r'\s'
 t_INV			= r'inventory' #do we want this?
+t_NOUN			= r'noun '"""nouns should be everything in a room"""
+
+
+"""Make room object that contains all parts of the room?
+
+noun would need to then be all parts of that room? 
+what if we want multiple different parts to a room?
+
+"""
 
 #and what of Nouns? 
 
