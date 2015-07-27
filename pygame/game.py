14c268f11d18aa403d35f5cf5eba05b0a975009b
Joey Walker <walkerj1@wit.edu>
2015-07-27 01:33:20 -0400

    some parsing work

--- pygame/game.py
+++ pygame/game.py
@@ -6,10 +6,15 @@
 #	Made using ply http://www.dabeaz.com/ply/
 # -----------------------------------------------------------------------------
 
+import sys
 
+# Example class
+class Human(object):
+	pass
+	
 #Our different token categories
 tokens = (
-    'MOVE', 'TALK', 'LOOK', 'JOIN', 'WHITESPACE'
+    'MOVE', 'TALK', 'LOOK', 'JOIN', 'WHITESPACE', 'QUIT', 'INV'
     )
 		
 # Our tokens
@@ -18,25 +23,44 @@ t_TALK			= r'talk|speak|chat|tell'
 t_LOOK			= r'look|glance'
 t_JOIN			= r'to|at|and' # I walked to him
 t_WHITESPACE	= r'\s'
+t_INV			= r'inventory' #do we want this?
+
+#and what of Nouns? 
+
+def t_QUIT(t):
+	r'quit'
+	s = input("Are you sure you want to quit?: ")
+	if (s == "yes" or s == "y" or s == "Yes"):
+		sys.exit()
 
 #Lexing errors
 def t_error(t):
 	print("error: bad characters: '%s'" % t.value[0])
 	t.lexer.skip(1)
 
-# Example class
-class Human(object):
-	pass		
-		
+#parsing
+	""" Parsing?
+
+def p_statement_action(p):
+	'''action : MOVE JOIN NOUN
+			  | TALK JOIN NOUN
+			  | LOOK JOIN NOUN
+	
+"""
+
+def p_error(p):
+	raise TypeError("Wasn't able to parse: %r" % (p.value,))
+
+
 #Main	
 import ply.lex as lex
 lexer = lex.lex()
 
 import ply.yacc as yacc
 #yacc.yacc()
+
 s = input("Action > ")
 lexer.input(s)
-
 
 while True:
     tok = lexer.token()
