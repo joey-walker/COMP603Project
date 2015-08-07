f508c5662fdd266a2d6e1df74e0a8497fb9b6226
Joey Walker <walkerj1@wit.edu>
2015-08-07 03:15:09 -0400

    some work

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------------------------
-# Dreadnought Khamzor: A prelude to Space and Time
+# Dreadnought Kamzhor: A prelude to Space and Time
 #
 # Authors: Joseph Walker, Sarthak Khatiwada, Ori Maci, Deep Patel, Chaskarandeep Singh
 #
@@ -17,6 +17,8 @@ class Areas(Enum):
 # Example class
 # Player, should hold onto a inventory(if we want), location of player
 class Player(object):
+	points = 0
+	name = None
 	def __init__(self,current_location):
 		self.current_location = current_location
 	def set_name(self,name):
@@ -29,12 +31,93 @@ Should contain the state of the room determining player progress in the area, eg
 until player completes puzzle, connector remains locked.
 """
 #########Our Rooms##########
-
-def room1():
-	print("something")
+import textwrap
+def introductorymessage():
+	print (textwrap.dedent("""\
 	
-def room2():
-	print("something else")
+							Dreadnought Kamzhor: A prelude to Space and Time
+							
+							by: Joseph Walker, Sarthak Khatiwada, Ori Maci, Deep Patel, 
+							    and Chaskarandeep Singh
+							
+							COMP603 Project
+							____________________________________________________________
+							
+							
+							You awaken to the sound of thunder.  
+							
+							Your eyes open up, your alarm clock replays an audio clip of thunder 
+							from a terrestrial planet.
+														
+							You motion your hand over the floating night stand and the room brightens
+							allowing you to finally see.
+							
+							You then proceed to punch the alarm clock in order to turn it off, as is 
+							typical in the far distant future.
+							
+							You are a lieutenant on board the vessel Dreadnought Kamzhor, 
+							you are scheduled to meet your captain on the bridge in a short while.
+							
+							Yet first you must first survive the perils that is leaving your room.
+							
+							You can talk at things, move to things, or look at things.
+
+							"""))
+
+
+def room1(p):
+	#print(p[0])	
+	if(p[1].lower()=="look"):
+		if(p[2].lower() == "room"):
+			print("You examine your room, it is a nice room; it contains a door which operates as an obvious exit.\nA computer's with its screen alight is waiting for user input.")
+		elif(p[2].lower() == "alarm"):
+			print("wow")
+		elif(p[2].lower() == "door"):
+			print("You examine the door.\nIts a monolithic sort of thing, as are all doors on Dreadnought Kamzhor.")
+		elif(p[2].lower() == "computer"):
+			print("wowcomp")
+		elif(p[2].lower() == "joey"):
+			print("Stop breaking the fourth wall.")
+		else: 
+			print("You don't see %s in the room." % p[2])
+			
+	if(p[1].lower()=="move"):
+		if(p[2].lower() == "room"):
+			print("You are already in the room")
+		elif(p[2].lower() == "alarm"):
+			print("There is no point in moving to your alarm, now follow the damn script.")
+		elif(p[2].lower() == "door"):
+			print("wowdoor")
+		elif(p[2].lower() == "computer"):
+			print("wowcomp")
+		elif(p[2].lower() == "joey"):
+			player.points -= 10
+			print("You cannot move the narrator, as hard as you try.  You lose ten points.\nPoint count: %s" % player.points)
+		else: 
+			print("You can't move to %s." % p[2])
+	
+	if(p[1].lower()=="talk"):
+		if(p[2].lower() == "room"):
+			print("You talk to the room,\nunfortunately this isn't a fancy room with a built in AI.\nSo it does not respond.\nYou ponder receiving medical help for your apparent schizophrenia.")
+		elif(p[2].lower() == "alarm"):
+			print("You speak to the alarm, chatting of lofty philosophical ideas,\nit stares back at you in awkward silence.")
+		elif(p[2].lower() == "door"):
+			if player.name is None:
+				print("You try speaking to the door, but the moment you open mouth it screeches at you:\n\"ERROR, OLD NAME LOST FOR WHATEVER REASON.\nINPUT NEW NAME AT YOUR COMPUTER TERMINAL\"\n")
+			else:
+				print("The door screeches once again as you try to speak:\n \"NAME CONFIRMED, YOU MAY PROCEED LIEUTENANT %s\"" % player.name)
+		elif(p[2].lower() == "computer"):
+			print("wowcomp")
+		elif(p[2].lower() == "joey"):
+			print("Stop talking to me.")
+		else: 
+			print("You can't talk to %s."% p[2])
+			
+			
+def room2(p):
+	print("\nUnfortunately you woke up too late,\nand the moment you stepped outside of your door,\na tall robot shot you in the face.")
+	print("\n\nThe End.")
+	sys.exit()
 
 ############################
 
@@ -64,16 +147,6 @@ def t_WHITESPACE(t):
 def t_DEFINITEART(t):
 	r'(?i)the'
 	return t
-	
-"""nouns should be everything in a room"""
-
-
-"""Make room object that contains all parts of the room?
-
-noun would need to then be all parts of that room? 
-what if we want multiple different parts to a room?
-
-"""
 
 def t_QUIT(t):
 	r'(?i)quit'
@@ -98,10 +171,8 @@ def p_statement(p):
 		else:
 			return
 
-	print(p[1])
-	print(p[2])
 	p[0] = p[1] + " " + p[2]
-	print(p[0])	
+	room_functions[player.current_location](p)
 
 def p_statement_action(p):
 	'''action  : MOVE JOIN
@@ -138,15 +209,15 @@ import logging
 log = logging.getLogger() """
 
 ## Start Game instructions
-
 player = init_player()
+introductorymessage()
+
+
 ### How to do switch case
 
-room_functions[player.current_location]()
+#player.current_location = Areas.area2
 
-player.current_location = Areas.area2
-
-room_functions[player.current_location]()
+#room_functions[player.current_location]()
 
 ###
 
